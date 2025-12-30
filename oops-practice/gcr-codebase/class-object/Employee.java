2bf13ad13886781d2900eaf3989a34a11fa2d02d
sonamsharma235 <trigunayatsonam@gmail.com>
2025-12-30 19:02:08 +0530

    [Sonam Sharma]Add. add employee program to oops-practice branch in gcr-codebase folder

---
+++ oops-practice/gcr-codebase/class-object/Employee.java
@@ -0,0 +1,27 @@
+class Employee {
+
+    String name;
+    int id;
+    double salary;
+
+    // Method to display employee details
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee id: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+
+        // Creating Employee object
+        Employee emp = new Employee();
+
+        // Assigning values
+        emp.name = "Rohan";
+        emp.id = 1;
+        emp.salary = 500000;
+
+        // Displaying details
+        emp.displayDetails();
+    }
+}
