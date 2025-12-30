d9714c7cae9a11247b531cd414dc2ff6f3046464
sonamsharma235 <trigunayatsonam@gmail.com>
2025-12-30 15:58:59 +0530

    [Sonam Sharma]Refactor. done with Use Case 4 and 5 of snakeLadder Problem

--- core-java-practice/scenerio-based/SnakeLadder.java
+++ core-java-practice/scenerio-based/SnakeLadder.java
@@ -18,6 +18,26 @@ public class SnakeLadder {
         int snake=2;
         int CountOfDie=1;
 
+        //UC 4 & UC 5
+        while(posOfplayer1!=100){
+           die=random.nextInt(6)+1; 
+           switch(CheckOption){
+            case 1:
+                posOfplayer1+=die;
+                if(posOfplayer1>100) 
+                    posOfplayer1-=die;
+                break;
+            case 2:
+                posOfplayer1-=die;
+                break;   
+          }
+          if(posOfplayer1<0){
+            posOfplayer1=0;
+          }
+          System.out.println(player1);
+          CountOfDie++;
+        }
+        
        
     }
     
