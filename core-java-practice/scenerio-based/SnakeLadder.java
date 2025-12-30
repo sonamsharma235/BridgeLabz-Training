e7db252fcf986e9fcd021b63446fdfc058cbbb1d
sonamsharma235 <trigunayatsonam@gmail.com>
2025-12-30 16:00:00 +0530

    [Sonam Sharma]Refactor. done with Use Case 7 of snakeLadder Problem

--- core-java-practice/scenerio-based/SnakeLadder.java
+++ core-java-practice/scenerio-based/SnakeLadder.java
@@ -4,7 +4,7 @@ public class SnakeLadder {
 
         //UC 1
         int posOfplayer1=0;
-        //UC 2
+         //UC 2
         Random random=new Random();
         int die=random.nextInt(6)+1;
         
@@ -37,6 +37,48 @@ public class SnakeLadder {
         //UC 6
         System.out.println(CountOfDie);
          
+        //UC 7
+        int ladder=1;
+        int snake=2;
+        int posOfPlayer2=0;
+        int posOfPlayer3=0;
+        boolean flag=true;
+        while(posOfPlayer2!=100 && posOfPlayer3!=100){
+              die=random.nextInt(6)+1; 
+              switch(CheckOption){
+                 case 1:
+                      if(flag==true){
+                       posOfPlayer2+=die;
+                       if(posOfPlayer2>100){
+                        posOfPlayer2-=die;
+                       }
+                    }else{
+                        posOfPlayer3+=die;
+                        if(posOfPlayer3>100){
+                        posOfPlayer3-=die;
+                       }
+                    }
+                    break;
+                 case 2:
+                       if(flag==true){
+                       posOfPlayer2-=die;
+                       }else{
+                        posOfPlayer3-=die;
+                       } 
+                      break; 
+              }
+              flag=!flag;
+              if(posOfPlayer2<0){
+                 posOfPlayer2=0;
+              }else if(posOfPlayer3<0)posOfPlayer3=0;
+
+             System.out.println(posOfPlayer2+" "+posOfPlayer3);
+      } 
+      if(posOfPlayer2==100){
+        System.out.println("Player 2 is the Winner");
+      }else{
+        System.out.println("Player 3 is the winner");
+      }
         
     }
     
