b97fe16f4b3b00b573ba205249c36d8135bbc23f
sonamsharma235 <trigunayatsonam@gmail.com>
2026-01-17 15:24:16 +0530

    [Sonam Sharma]Add. added UseCase-3 to the addressBook  folder

--- oops-practice/gcr-codebase/addressBook/ContactService.java
+++ oops-practice/gcr-codebase/addressBook/ContactService.java
@@ -33,6 +33,62 @@ public class ContactService {
     	
     }
     
+	// UseCase-3: edit contact
+	//Edit contacts
+    public void editContacts(String name) {
+    	boolean flag=true;
+    	for(ContactModel c:dao.addressBook) {
+    		if(c.getFirstName().equals(name)) {
+    				System.out.println("1: edit first name");
+    				System.out.println("2: edit Last name");
+    				System.out.println("3: edit Address");
+    				System.out.println("4: edit City");
+    				System.out.println("5: edit ZIP");
+    				System.out.println("6: edit Phone number");
+    				System.out.println("7: edit email");
+    				System.out.println("choose what to edit ");
+    				int choose =sc.nextInt();
+    				flag=false;
+    				
+    				switch(choose) {
+    				   case 1:System.out.println("enter first Name");
+    				          String first=sc.next();
+    					      c.setFirstName(first);
+    					      break;
+    					      
+    				   case 2:System.out.println("enter last Name");
+					          c.setLastName(sc.next());
+					          break;
+					          
+    				   case 3:System.out.println("enter new Address");
+					          c.setAddress(sc.next());
+					          break;
+					          
+    				   case 4:System.out.println("enter new city");
+					          c.setCity(sc.nextLine());
+					          break;
+					          
+    				   case 5:System.out.println("enter new zip");
+					          c.setZip(sc.nextInt());
+					          break;
+					         
+    				   case 6:System.out.println("enter new phone number");
+					          c.setPhoneNo(sc.nextLong());
+					          break;
+					          
+    				   case 7:System.out.println("enter new Email");
+					          c.setEmail(sc.nextLine());
+					          break;        
+    				} 
+    				
+    			}
+    			
+    		}
+    	if(flag==true) {
+    		System.out.println("enter valid name");
+    	}
+    }
+    
     //display contacts 
     public void displayContacts() {
     	for(ContactModel c:dao.addressBook) {
