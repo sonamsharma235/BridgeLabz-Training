c748e1754e36a940586d90846da5171a5860facf
sonamsharma235 <trigunayatsonam@gmail.com>
2026-02-16 15:01:13 +0530

    [Sonam Sharma]Add. added useCase 8 to employeeWage problem in scenerio based folder

--- core-java-practice/scenerio-based/EmployeeWageComputation.java
+++ core-java-practice/scenerio-based/EmployeeWageComputation.java
@@ -1,23 +1,20 @@
-import java.util.*;
-public class EmployeeWageComputation{
+import java.util.Random;
 
-    //UC 7
-    // Class Variables (Static Variables)
-    static final int IS_PART_TIME = 1;
-    static final int IS_FULL_TIME = 2;
-    static final int WAGE_PER_HOUR = 20;
-    static final int MAX_WORKING_DAYS = 20;
-    static final int MAX_WORKING_HOURS = 100;
-
-    // Class Method (Static Method)
-    public static int computeEmployeeWage() {
+public class EmployeeWageComputation {
+     
+    // useCase 8
+    // Class Method with Parameters
+    public static void computeEmployeeWage(String company,
+                                           int wagePerHour,
+                                           int maxWorkingDays,
+                                           int maxWorkingHours) {
 
         int totalEmpHours = 0;
         int totalWorkingDays = 0;
         Random random = new Random();
 
-        while (totalEmpHours < MAX_WORKING_HOURS &&
-               totalWorkingDays < MAX_WORKING_DAYS) {
+        while (totalEmpHours < maxWorkingHours &&
+               totalWorkingDays < maxWorkingDays) {
 
             totalWorkingDays++;
 
@@ -25,101 +22,37 @@ public class EmployeeWageComputation{
             int empHours = 0;
 
             switch (empType) {
-                case IS_PART_TIME:
-                    empHours = 4;
+                case 1:
+                    empHours = 4;  // Part Time
                     break;
 
-                case IS_FULL_TIME:
-                    empHours = 8;
+                case 2:
+                    empHours = 8;  // Full Time
                     break;
 
                 default:
-                    empHours = 0;
+                    empHours = 0;  // Absent
             }
 
             totalEmpHours += empHours;
         }
 
-        return totalEmpHours * WAGE_PER_HOUR;
-    }
-    public static void main(String []args){
-        //UC 1  
-        System.out.println("Welcom To Employee Wage Computation");
+        int totalWage = totalEmpHours * wagePerHour;
 
-        //UC 2
-        int is_present=1;
-        Random random =new Random();
-        int attendence=random.nextInt(2);
-        if(attendence==is_present){
-            System.out.println("present");
-        }else{
-            System.out.println("not present");
-        }
-
-        //UC 3
-        // int wage_per_hour=20;
-        int full_day_hour=8;
-        if(attendence==is_present){
-            int daily_wage=wage_per_hour*full_day_hour;
-            System.out.println("daily wage "+daily_wage);
-        }else{
-            System.out.println("employee is absent");
-            System.out.println("wage is =0");
-        }
-
-        //UC 4
-
-        // int is_part_time=1;
-        // int is_full_time=2;
-        int empHours=0;
-
-        int empType=random.nextInt();
-
-        switch(empType){
-            case is_part_time:
-                empHours=4;
-                System.out.println("part time employee");
-                break;
-            case is_full_time:
-                empHours=8;
-                System.out.println("full time employee");
-                break;    
-            default:
-                empHours=0;
-                System.out.println("employee absent");    
-        }
-
-        int dailyWage=empHours*wage_per_hour;
-        System.out.println("daily wage is "+ dailyWage);
-
-        //UC 5
-        int monthlyWage=0;
-        int working_Days=sc.nextInt();
-
-        for(int day=1;day<=working_Days;day++){
-          empType=random.nextInt();
-          switch(empType){
-            case is_part_time:
-                empHours=4;
-                System.out.println("part time employee");
-                break;
-            case is_full_time:
-                empHours=8;
-                System.out.println("full time employee");
-                break;    
-            default:
-                empHours=0;
-                System.out.println("employee absent");    
-         }
-            daily_wage=empHours*wage_per_hour;
-            monthlyWage+=daily_wage;
-        }
-        System.out.println("monthly wage ="+monthlyWage);
-
-        int totalWage=computeEmployeeWage();
-
+        System.out.println("Company: " + company);
         System.out.println("Total Working Days: " + totalWorkingDays);
         System.out.println("Total Working Hours: " + totalEmpHours);
-        System.out.println("Total Monthly Wage: " + totalWage)
+        System.out.println("Total Wage: " + totalWage);
+        System.out.println("-----------------------------------");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome To Employee Wage Computation");
+
+        // Multiple Companies
+        computeEmployeeWage("TCS", 20, 20, 100);
+        computeEmployeeWage("Infosys", 25, 22, 120);
+        computeEmployeeWage("Wipro", 18, 25, 110);
     }
 }
